b05bdd2eaa88f5a4b1c4e823f53b077abc8199bb
Peakkay <prachet.09.10@gmail.com>
2024-10-13 17:03:10 +0530

    Update Choice.cs

--- FreakyDev-Arcadia/Assets/Scripts/Classes/Choice.cs
+++ FreakyDev-Arcadia/Assets/Scripts/Classes/Choice.cs
@@ -7,7 +7,7 @@ public class Choice
 {
     public string choiceText;  // Text displayed for the choice
     public int corruptionImpact;  // Change in corruption from this choice
-    public Choice statImpact;  // Impact on player's stats
+    public Stat statImpact;  // Impact on player's stats
     public Spell newSpell;  // A spell the player might gain from this choice
     public Reality targetReality; // Reality shift from this choice, if any
     public Quest newQuest;  // New quest to start from this choice
